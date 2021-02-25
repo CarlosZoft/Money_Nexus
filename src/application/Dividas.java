270e3d0f5337fcadf417bcccfb75f9680fde20e0
Carlos <carlosoftwar@gmail.com>
2021-02-25 00:50:44 -0300

    Create : Class Dividas

---
+++ src/application/Dividas.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Dividas extends Categoria{
+    private int tipo;
+
+    public Dividas(String titulo, String descricao,double valor) {
+        super(titulo, descricao, valor);
+    }
+
+
+}
