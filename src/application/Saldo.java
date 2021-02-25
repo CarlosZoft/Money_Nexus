d503c12c3424a664345d8175beb5f59123505be8
Carlos <carlosoftwar@gmail.com>
2021-02-25 00:51:05 -0300

    Create : Class Saldo

---
+++ src/application/Saldo.java
@@ -0,0 +1,8 @@
+package application;
+
+public class Saldo extends Categoria{
+
+    public Saldo(String titulo, String descricao, double valor) {
+        super(titulo, descricao, valor);
+    }
+}
