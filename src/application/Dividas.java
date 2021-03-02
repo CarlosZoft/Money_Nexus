0556f76e5b0cc208515ebe3ac486f5e3fdbcbad7
Carlos <carlosoftwar@gmail.com>
2021-03-02 17:11:26 -0300

    Update : Dividas class

--- src/application/Dividas.java
+++ src/application/Dividas.java
@@ -3,8 +3,9 @@ package application;
 public class Dividas extends Categoria{
     private int tipo;
 
-    public Dividas(String titulo,double valor) {
+    public Dividas(String titulo, double valor, int tipo) {
         super(titulo, valor);
+        this.tipo = tipo;
     }
 
 
