c901716496acf88534aec2646c76c49ebb87585d
Carlos <carlosoftwar@gmail.com>
2021-03-20 14:13:30 -0300

    Update : Adicionando comentarios e Sobrecarga

--- src/application/Models/Saldo.java
+++ src/application/Models/Saldo.java
@@ -2,10 +2,17 @@ package application.Models;
 
 import application.Models.Categoria;
 
+// Herda class Categoria
 public class Saldo extends Categoria {
 
     public Saldo(String titulo, double valor) {
 
         super(titulo, valor);
+    }
+
+    //Exemplo sobrecarga;
+    @Override
+    public String getTitulo() {
+        return super.getTitulo();
     }
 }
