3efc24d28b7a827063cc211f175811f2f0e3eba5
Carlos <carlosoftwar@gmail.com>
2021-03-21 11:06:50 -0300

    Update : Adicionando Valor atual

--- src/application/Models/Economia.java
+++ src/application/Models/Economia.java
@@ -5,9 +5,19 @@ import application.Models.Categoria;
 // Herda class Categoria
 public class Economia extends Categoria {
 
+    private double valorAtual;
+
     public Economia(String titulo,double valor) {
 
         super(titulo, valor);
+    }
+
+    public double getValorAtual() {
+        return valorAtual;
+    }
+
+    public void setValorAtual(double valorAtual) {
+        this.valorAtual = valorAtual;
     }
 
 }
