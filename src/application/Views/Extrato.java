d1ba38ed43f85c21f31e2adb55016e151e34a3b4
Carlos <carlosoftwar@gmail.com>
2021-04-10 21:29:38 -0300

    Update : extrato form

---
+++ src/application/Views/Extrato.java
@@ -0,0 +1,20 @@
+package application.Views;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Extrato {
+    private JButton menuButton;
+    private JTextArea mostraExtrato;
+    private JLabel extrato;
+
+    public Extrato() {
+        menuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
