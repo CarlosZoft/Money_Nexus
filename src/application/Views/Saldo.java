07a5b6aec1f53120c49ab8e9216b6a46e4e818cf
Carlos <carlosoftwar@gmail.com>
2021-04-10 21:30:02 -0300

    Update : Saldo form/listener

---
+++ src/application/Views/Saldo.java
@@ -0,0 +1,35 @@
+package application.Views;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Saldo {
+    private JList listSaldo;
+    private JButton adicionarButton;
+    private JButton excluirButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton menuButton;
+
+    public Saldo() {
+        excluirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        menuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        adicionarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
