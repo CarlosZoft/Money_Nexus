7da27c682157da60e6a00e1fe695658dce7fdfe2
Carlos <carlosoftwar@gmail.com>
2021-04-13 19:01:39 -0300

    Add : Adicionando Classe para Validar entrada

---
+++ src/application/errors/Valida.java
@@ -0,0 +1,56 @@
+package application.errors;
+import application.Controllers.*;
+
+public class Valida {
+    public FeedBack validaSaldo(String titulo, double valor) {
+        FeedBack resposta = new FeedBack("Saldo valido !!", true);
+        if(titulo instanceof String && titulo.length() > 3){
+            if(valor > 0){
+                return resposta;
+            }
+            else{
+                resposta.setMensagem("Valor invalido !!");
+                resposta.setStatus(false);
+                return resposta;
+            }
+        }
+        else {
+            resposta.setMensagem("Titulo Invalido !!");
+            resposta.setStatus(false);
+            return resposta;
+        }
+
+    }
+
+    public FeedBack validaTitulo(String titulo) {
+        FeedBack resposta = new FeedBack("titulo valido!!", true);
+        if(titulo instanceof String && titulo.length() > 3){
+            return resposta;
+        }
+        else {
+            resposta.setMensagem("Titulo Invalido: Adicione mais caracteres");
+            resposta.setStatus(false);
+            return resposta;
+        }
+    }
+
+    public FeedBack validaValor(double valor) {
+        FeedBack resposta = new FeedBack("Valor Valido!!", true);
+        if(valor > 0){
+            return resposta;
+        }
+        else {
+            resposta.setMensagem("Valor Invalido : Não coloque valor negativo");
+            resposta.setStatus(false);
+            return resposta;
+        }
+    }
+
+
+
+
+
+
+
+
+}
