bd92f037fcc5d4469d26163d6f734e96d111d75e
Carlos <carlosoftwar@gmail.com>
2021-04-13 19:00:11 -0300

    Update : tratando Economia de outra maneira

--- src/application/Controllers/ControllerEconomia.java
+++ src/application/Controllers/ControllerEconomia.java
@@ -3,6 +3,8 @@ package application.Controllers;
 import java.util.ArrayList;
 import application.Models.*;
 import java.time.LocalDate;
+import java.util.Locale;
+
 /**
  * Controller Para Economias
  *
@@ -17,20 +19,63 @@ public class ControllerEconomia {
 
 
     // METHODS
+    public void AdicionaEconomia(String titulo, double valor, int meta) {
+        String tituloaux = titulo.toLowerCase();
+        int flag = 0;
+        for(int i = 0; i<getSize(); ++i){
+            Economia tituloPesquisa = this.Eco.get(i);
+            if(tituloPesquisa.getTitulo() == tituloaux) {
+                this.Eco.get(i).setValor(tituloPesquisa.getValor() + valor);
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0){
+            Economia aux  = new Economia(tituloaux,valor,meta);
+            this.Eco.add(aux);
+        }
+    }
 
+    public void editaEconomia(String titulo, double valor, int index) {
+        String tituloaux = titulo.toLowerCase();
+        this.Eco.get(index).setTitulo(tituloaux);
+        this.Eco.get(index).setValor(valor);
+    }
+
+    public void editaTituloEconomia(String titulo, int index) {
+        String tituloaux = titulo.toLowerCase();
+        this.Eco.get(index).setTitulo(tituloaux);
+    }
+
+    public void editaValorEconomia(double valor, int index) {
+        this.Eco.get(index).setValor(valor);
+    }
+
+    public void removeEconomia(int index) {
+        this.Eco.remove(index);
+    }
+
+    public void clearEconomia() {
+        this.Eco.clear();
+    }
     public int getSize() {
         return this.Eco.size();
     }
 
-    public ArrayList<Economia> getAll(){
-        return this.Eco;
+    public ArrayList<String> getAllEconomias(){
+        ArrayList<String> aux = new ArrayList<String>();
+
+        for(int i = 0 ; i<this.getSize(); ++i){
+            aux.add(this.Eco.get(i).toString());
+        }
+        return aux;
     }
 
     public ArrayList<String> getTitulos(){
 
         ArrayList <String> aux = new ArrayList<String>();
 
-        for(int i = 0; i < this.getSize(); i++) {
+        for(int i = 0; i < this.getSize(); ++i) {
             aux.add(this.Eco.get(i).getTitulo());
         }
 
@@ -41,7 +86,7 @@ public class ControllerEconomia {
 
         ArrayList <Double> aux = new ArrayList<Double>();
 
-        for(int i = 0; i < this.getSize(); i++) {
+        for(int i = 0; i < this.getSize(); ++i) {
             aux.add(this.Eco.get(i).getValor());
         }
 
