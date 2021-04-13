ba9a952bfb85fc6c0e7c4fc95b2e789c253490df
Carlos <carlosoftwar@gmail.com>
2021-04-13 01:35:08 -0300

    Update : Atualizando Saldo View - Adicionando Saldo total

--- src/application/Views/SaldoView.java
+++ src/application/Views/SaldoView.java
@@ -1,30 +1,24 @@
 package application.Views;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Saldo {
+public class SaldoView {
     private JList listSaldo;
     private JButton adicionarButton;
     private JButton excluirButton;
     private JTextField textField1;
     private JTextField textField2;
     private JButton menuButton;
+    private JLabel saldoTotal;
+    public JPanel panel1;
 
-    public Saldo() {
-        excluirButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+    public SaldoView() {
 
-            }
-        });
-        menuButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
 
-            }
-        });
+        saldoTotal.setText("1789.9");
         adicionarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -32,4 +26,5 @@ public class Saldo {
             }
         });
     }
+
 }
