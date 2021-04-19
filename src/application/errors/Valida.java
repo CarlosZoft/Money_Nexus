526aaf0394c0992d10f12467cd33b338a29c0794
Carlos <carlosoftwar@gmail.com>
2021-04-18 23:32:05 -0300

    adicionando novos tratamentos

--- src/application/errors/Valida.java
+++ src/application/errors/Valida.java
@@ -2,15 +2,32 @@ package application.errors;
 import application.Controllers.*;
 
 public class Valida {
+    
+    
+    
+    public String Erro(int tipo){
+        if(tipo==1)
+            return "Selecione um item da lista";
+        
+     
+        return "nada";    
+    }
+    
+    /**
+     *
+     * @param titulo Titulo do saldo
+     * @param valor Valor do saldo
+     * @return FeedBack - True ou False
+     */
     public FeedBack validaSaldo(String titulo, double valor) {
         FeedBack resposta = new FeedBack("Saldo Adicionado com Sucesso !!", true);
-        if(titulo instanceof String && titulo.length() > 2){
-            if(valor > 0) {
+        if(titulo instanceof String && titulo.length() > 2 && !titulo.equals("Descricao")){
+            if(valor <= 0) {
+                resposta.setMensagem("Valor invalido !!");
+                resposta.setStatus(false);
                 return resposta;
             }
             else {
-                resposta.setMensagem("Valor invalido !!");
-                resposta.setStatus(false);
                 return resposta;
             }
         }
@@ -22,6 +39,11 @@ public class Valida {
 
     }
 
+    /**
+     *
+     * @param titulo Titulo do Saldo;
+     * @return FeedBack - True ou Falso
+     */
     public FeedBack validaTitulo(String titulo) {
         FeedBack resposta = new FeedBack("titulo valido!!", true);
         if(titulo instanceof String && titulo.length() > 2){
@@ -34,8 +56,13 @@ public class Valida {
         }
     }
 
+    /**
+     *
+     * @param valor Valor a ser Adicionado
+     * @return
+     */
     public FeedBack validaValor(double valor) {
-        FeedBack resposta = new FeedBack("Valor Valido!!", true);
+        FeedBack resposta = new FeedBack("Valor Adicionado!!", true);
         if(valor > 0){
             return resposta;
         }
@@ -45,8 +72,16 @@ public class Valida {
             return resposta;
         }
     }
+
+    /**
+     *
+     * @param valor Campo String a ser verificada
+     * @return True ou false
+     */
     public boolean verificaString(String valor){
-        
+        if(valor.isBlank()){
+            return false;
+        }
         for(int i = 0 ; i < valor.length(); ++i){
            char aux = valor.charAt(i);
            if((aux >= 65 && aux <= 90) || (aux >= 97 && aux <= 122)){
